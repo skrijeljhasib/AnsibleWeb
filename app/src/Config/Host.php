89fe2b988c28366331c1d78bb07bd52735eb4edb
skrijeljhasib <h.skrijelj@flash-global.net>
2017-05-03 10:56:07 +0200

    config file for openstack authentification

---
+++ app/src/Config/Host.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: skrijeljhasib
+ * Date: 02.05.17
+ * Time: 10:32
+ */
+
+namespace Project\Config;
+
+
+use ObjectivePHP\Config\SingleValueDirective;
+
+class Host extends SingleValueDirective
+{
+
+}
---
+++ app/src/Config/MachineTemplate.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: skrijeljhasib
+ * Date: 27.04.17
+ * Time: 14:55
+ */
+
+namespace Project\Config;
+
+
+use ObjectivePHP\Config\SingleValueDirective;
+
+class MachineTemplate extends SingleValueDirective
+{
+
+}
