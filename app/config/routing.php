db6f1402153bbb903ef9ca779a7a53b407e4e8ce
skrijeljhasib <h.skrijelj@flash-global.net>
2017-05-03 11:02:16 +0200

    PlaybookGateway class will be invoked if a route to /playbookJSON is executed

--- app/config/routing.php
+++ app/config/routing.php
@@ -3,22 +3,12 @@
 use ObjectivePHP\Package\FastRoute\Config\FastRoute;
 use Project\Action\Home;
 use Project\Action\InstallServer;
+use Project\Application;
+use Project\Gateway\PlayBookGateway;
 
 return [
         // route aliasing
-        new FastRoute('home', '/', Home::class),
-        new FastRoute('installServer', '/installServer', InstallServer::class),
-        new FastRoute('post_data','/post_data', function() {
-            $ch = curl_init('http://localhost:8080/post_data');
-            curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-            curl_setopt($ch, CURLOPT_POSTFIELDS, file_get_contents('php://input'));
-            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-            curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-                    'Content-Type: application/json',
-                    'Content-Length: ' . strlen(file_get_contents('php://input'))
-                )
-            );
-
-            echo curl_exec($ch);
-        },FastRoute::POST),
+        new FastRoute('home', '/', Home::class,FastRoute::GET),
+        new FastRoute('installServer', '/installServer', InstallServer::class,FastRoute::GET),
+        new FastRoute('playBookJSON', '/playBookJSON', PlayBookGateway::class,FastRoute::GET),
 ];
