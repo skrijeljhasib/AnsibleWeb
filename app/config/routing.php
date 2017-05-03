f1f8a0573186c18d8c0eac082ef82e3951fe5a6d
skrijeljhasib <h.skrijelj@flash-global.net>
2017-05-03 15:12:15 +0200

    name changed

--- app/config/routing.php
+++ app/config/routing.php
@@ -2,13 +2,13 @@
 
 use ObjectivePHP\Package\FastRoute\Config\FastRoute;
 use Project\Action\Home;
-use Project\Action\InstallServer;
+use Project\Action\CreateMachine;
 use Project\Application;
 use Project\Gateway\PlayBookGateway;
 
 return [
         // route aliasing
         new FastRoute('home', '/', Home::class,FastRoute::GET),
-        new FastRoute('installServer', '/installServer', InstallServer::class,FastRoute::GET),
+        new FastRoute('createMachine', '/createMachine', CreateMachine::class,FastRoute::GET),
         new FastRoute('playBookJSON', '/playBookJSON', PlayBookGateway::class,FastRoute::GET),
 ];
---
+++ app/src/Action/CreateMachine.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: skrijeljhasib
+ * Date: 12.04.17
+ * Time: 11:15
+ */
+
+namespace Project\Action;
+
+use ObjectivePHP\Application\Action\RenderableAction;
+use ObjectivePHP\Application\ApplicationInterface;
+use ObjectivePHP\Application\View\Helper\Vars;
+use ObjectivePHP\Html\Exception;
+use Project\Config\Host;
+use Project\Entity\PlayBook;
+
+
+class CreateMachine extends RenderableAction
+{
+
+    /**
+     * @param ApplicationInterface $app
+     * @return array
+     * @throws Exception
+     */
+    function run(ApplicationInterface $app)
+    {
+        Vars::set('page.title', 'Create your machine');
+
+        $host = $app->getConfig()->get(Host::class);
+
+        try
+        {
+            $package_gateway = $app->getServicesFactory()->get('gateway.packages');
+            $packages = $package_gateway->fetch();
+        }
+        catch (Exception $e)
+        {
+            throw new Exception('Can not load packages from DB');
+        }
+
+        return compact('packages','host');
+    }
+
+}
