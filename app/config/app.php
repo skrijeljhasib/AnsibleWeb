ef88c68c3079ab0d8bd01abcbfe51450f12be0e2
skrijeljhasib <h.skrijelj@flash-global.net>
2017-05-03 10:55:13 +0200

    unused classes removed

--- app/config/app.php
+++ app/config/app.php
@@ -5,8 +5,6 @@ namespace Config;
 use ObjectivePHP\Application\Config\ActionNamespace;
 use ObjectivePHP\Application\Config\ApplicationName;
 use ObjectivePHP\Application\Config\LayoutsLocation;
-use ObjectivePHP\Matcher\Matcher;
-use ObjectivePHP\ServicesFactory\Config\Service;
 
 return [
     new ApplicationName('AnsibleWeb'),
