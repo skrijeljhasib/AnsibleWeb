cbdb29afd7fbf6ef2d337f4f85edfb762a3c2107
skrijeljhasib <h.skrijelj@flash-global.net>
2017-05-08 11:20:22 +0200

    Class Name changed

--- app/config/routing.php
+++ app/config/routing.php
@@ -3,12 +3,11 @@
 use ObjectivePHP\Package\FastRoute\Config\FastRoute;
 use Project\Action\Home;
 use Project\Action\CreateMachine;
-use Project\Application;
-use Project\Gateway\PlayBookGateway;
+use Project\Action\PlayBook;
 
 return [
         // route aliasing
         new FastRoute('home', '/', Home::class,FastRoute::GET),
         new FastRoute('createMachine', '/createMachine', CreateMachine::class,FastRoute::GET),
-        new FastRoute('playBookJSON', '/playBookJSON', PlayBookGateway::class,FastRoute::GET),
+        new FastRoute('playBookJSON', '/playBookJSON', PlayBook::class,FastRoute::GET),
 ];
