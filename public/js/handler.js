bc097a11c3903cad75d2e4ac7af9de2aa1d213c0
skrijeljhasib <h.skrijelj@flash-global.net>
2017-05-08 11:21:47 +0200

    Clean

--- public/js/handler.js
+++ public/js/handler.js
@@ -1,28 +1,43 @@
 $(document).ready(function () {
 
-    $("#selectAllorNot").click(function(){
-        if($("#selectAllorNot").is(':checked') ){
-            $('#packages').find('option').attr('selected', true).parent().trigger('change')
-        } else {
-            $('#packages').find('option').attr('selected', false).parent().trigger('change')
-        }
-    });
-
-    const $loading = $('#loading').hide();
+    const loading = $('#loading').hide();
     $(document)
-        .ajaxStart(function () {
-            $loading.show();
+        .ajaxStart(function ()
+        {
+            loading.show();
         })
-        .ajaxStop(function () {
-            $loading.hide();
+        .ajaxStop(function ()
+        {
+            loading.hide();
         });
 
-    $('#myTabs').find('a').click(function (e) {
-        e.preventDefault();
-        $(this).tab('show');
-    });
+    $('#myTabs').find('a').click(
+        function (e)
+        {
+            e.preventDefault();
+            $(this).tab('show');
+        }
+    );
 
-    $('select').select2({
-        placeholder: "Choose ..."
+    $('select').select2(
+        {
+            placeholder: "Choose ..."
+        });
+
+    $("#selectAllorNot").click(function()
+    {
+        if($("#selectAllorNot").is(':checked'))
+        {
+            var selectedItems = [];
+            var allOptions = $("#packages").find('option');
+            allOptions.each(function() {
+                selectedItems.push($(this).val());
+            });
+            $("#packages").val(selectedItems).trigger("change");
+        }
+        else
+        {
+            $('#packages').find('option').removeAttr('selected').parent().trigger('change');
+        }
     });
 });
