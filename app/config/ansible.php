064ad19b2ff1368bf6936438f25b284dfcb74290
skrijeljhasib <h.skrijelj@flash-global.net>
2017-05-10 10:49:40 +0200

    All ansible configuration moved to one file

--- app/config/ansible.php
+++ app/config/ansible.php
@@ -3,6 +3,10 @@
 namespace Config;
 
 use Project\Config\AnsibleApi;
+use Project\Config\Host;
+use Project\Config\MachineAccess;
+use Project\Config\OpenStackAuth;
+use Project\Config\MachineTemplate;
 
 
 $ansible_api = [
@@ -10,7 +14,38 @@ $ansible_api = [
     "tmp_file" => "/tmp/new_host.txt"
 ];
 
+$host = [
+    "host_config" => "CUSTOM", // FIXED, CUSTOM, RANDOM
+    "host_name" => "AnsibleWebTest" // Only used if $host_config is set to FIXED
+];
+
+$openstack_auth = [
+    "auth_url" => "auth_url",
+    "username" => "username",
+    "password" => "password",
+    "project_name" => "project_name"
+];
+
+$machine_template = [
+    "name" => $host['host_name'],
+    "key_name" => "ansiblekey",
+    "image" => "image",
+    "flavor" => "flavor",
+    "auto_ip" => "auto-ip",
+    "network" => "network",
+    "region_name" => "region_name",
+    "timeout" => "timeout"
+];
+
+$machine_access = [
+    "remote_user" => "remote_user"
+];
+
 
 return [
+    new OpenStackAuth($openstack_auth),
+    new MachineTemplate($machine_template),
+    new Host($host),
+    new MachineAccess($machine_access),
     new AnsibleApi($ansible_api)
 ];
