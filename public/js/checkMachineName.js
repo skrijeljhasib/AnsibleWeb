094ccd9bb2bcea0f1c77dd7b6e391fa718d6e8a5
skrijeljhasib <h.skrijelj@flash-global.net>
2017-05-15 19:46:51 +0200

    Add Please Wait Dialog

--- public/js/checkMachineName.js
+++ public/js/checkMachineName.js
@@ -1,49 +1,37 @@
 $(document).ready(function () {
+    let machines = [];
+    $('#pleaseWaitDialog').modal();
 
-    if($('#name').val())
-    {
-        let hostname = $("#name");
+    $.ajax({
+        type: 'GET',
+        url: '/playBookJSON',
+        data: {
+            playbook: 'getAllMachine'
+        }
+    }).done(function (data) {
 
-        hostname.attr("disabled", "disabled");
-
-        let machines = [];
+        let playbooks = '[';
+        playbooks += data;
+        playbooks += ']';
 
         $.ajax({
-            type: 'GET',
-            url: '/playBookJSON',
-            data: {
-                playbook: 'getAllMachine'
+            type: 'POST',
+            url: api_ip+'/post_data',
+            data: playbooks,
+            dataType: 'json'
+        }).done(function (response) {
+
+            for (let i = 0; i < response[0].ansible_facts.openstack_servers.length; i++)
+            {
+                machines.push(response[0].ansible_facts.openstack_servers[i].name);
             }
-        }).done(function (data) {
-
-            let playbooks = '[';
-            playbooks += data;
-            playbooks += ']';
-
-            $.ajax({
-                type: 'POST',
-                url: api_ip + '/post_data',
-                data: playbooks,
-                dataType: 'json'
-            }).done(function (response) {
-
-                for (let i = 0; i < response[0].ansible_facts.openstack_servers.length; i++)
-                {
-                    machines.push(response[0].ansible_facts.openstack_servers[i].name);
-                }
-
-                console.log(machines);
-
-                hostname.removeAttr("disabled");
-
-            }).fail(function (error) {
-                console.log(JSON.stringify(error));
-            });
 
         }).fail(function (error) {
             console.log(JSON.stringify(error));
         }).then(function () {
+            $('#pleaseWaitDialog').modal('hide');
 
+            let hostname = $('#name');
             hostname.keyup(function() {
                 if($.inArray(hostname.val(),machines) !== -1)
                 {
@@ -52,7 +40,9 @@ $(document).ready(function () {
                 }
             });
         });
-    }
 
+    }).fail(function (error) {
+        console.log(JSON.stringify(error));
+    })
 
 });
