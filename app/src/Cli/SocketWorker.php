b8182e89a9b04130fd15511e7dbbce2c3cde2cc9
skrijeljhasib <h.skrijelj@flash-global.net>
2017-05-29 20:52:36 +0200

    WebSocket broadcast on message

--- app/src/Cli/SocketWorker.php
+++ app/src/Cli/SocketWorker.php
@@ -4,16 +4,11 @@ namespace Project\Cli;
 
 use ObjectivePHP\Application\ApplicationInterface;
 use ObjectivePHP\Cli\Action\AbstractCliAction;
-use Project\Config\AnsibleApi;
-use Project\Config\MachineTemplate;
-use Project\Config\OpenStackAuth;
-use Project\Entity\DB\Host;
 use Hoa\Event\Bucket;
-use Pheanstalk\Pheanstalk;
 
 /**
  * Class SocketWorker
- * @package Project\Listener
+ * @package Project\Cli
  */
 class SocketWorker extends AbstractCliAction
 {
@@ -25,29 +20,30 @@ class SocketWorker extends AbstractCliAction
     
     public function run(ApplicationInterface $app)
     {
-	$ansible_api = $app->getConfig()->get(AnsibleApi::class);
+        $websocket = new \Hoa\Websocket\Server(
+                new \Hoa\Socket\Server('ws://0.0.0.0:9000')
+        );
 
-        $pheanstalk = new Pheanstalk($ansible_api['beanstalk']);
+        $websocket->on('open', function () {
+            echo 'new connection', "\n";
+                return;
+        });
 
-	$websocket = new \Hoa\Websocket\Server(
-    		new \Hoa\Socket\Server('ws://0.0.0.0:9000')
-	);
+        $websocket->on('message', function (Bucket $bucket) {
+            $data = $bucket->getData();
 
-	$websocket->on('open', function () {
-   		echo 'new connection', "\n";
-    		return;
-	});
+            echo 'message: ', $data['message'], "\n";
+            $bucket->getSource()->broadcast($data['message']);
 
-	$websocket->on('message', function (Bucket $bucket) {
-    		return;
-	});
-	
-	$websocket->on('close', function () {
-    		echo 'connection closed', "\n";
-    		return;
-	});
+            return;
+        });
 
-	$websocket->run();
+        $websocket->on('close', function () {
+                echo 'connection closed', "\n";
+                return;
+        });
+
+        $websocket->run();
 
     }
 }
