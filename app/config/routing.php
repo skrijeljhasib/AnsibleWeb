e70291ebdd0b0605395aff53e7e762166e5a35d9
lav <lvenier@flash-global.net>
2017-06-25 19:47:51 +0000

    add route for getallmachines

--- app/config/routing.php
+++ app/config/routing.php
@@ -4,10 +4,12 @@ use ObjectivePHP\Package\FastRoute\Config\FastRoute;
 use Project\Action\Home;
 use Project\Action\CreateMachine;
 use Project\Action\ListMachine;
+use Project\Action\GetAllMachine;
 
 return [
         // route aliasing
         new FastRoute('home', '/', Home::class, FastRoute::GET),
         new FastRoute('createMachine', '/createMachine', CreateMachine::class, FastRoute::GET),
         new FastRoute('listMachine', '/listMachine', ListMachine::class, FastRoute::GET),
+	new FastRoute('getAllMachine', '/getAllMachine', ListMachine::class, FastRoute::GET),
 ];
