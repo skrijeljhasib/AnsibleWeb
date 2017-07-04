90fdcbb9192a5a23bc7b465b2198e966a2469325
lav <lvenier@flash-global.net>
2017-07-04 17:12:38 +0200

    add steps in depencies service

--- app/src/Service/InstallDependenciesService.php
+++ app/src/Service/InstallDependenciesService.php
@@ -23,9 +23,10 @@ class InstallDependenciesService
     public function load($machine_access, $app_get)
     {
         $ip = $app_get->get('ip');
+	$step = $app_get->get('step');
         $playbook = new PlayBook();
 
-        $playbook->setName('Install dependencies');
+        $playbook->setName('Install dependencies (' . $step . ')');
         $playbook->setHosts($ip);
         $playbook->setConnection('ssh');
         $playbook->setRemoteUser($machine_access['remote_user']);
@@ -34,8 +35,20 @@ class InstallDependenciesService
         $playbook->setBecomeUser('root');
         $playbook->setGatherFacts('false');
 
-        $raw = new Raw();
-        $raw->setRaw('apt-get update ; apt-get upgrade -y; apt-get install -y python-apt aptitude');
+	if ($step == 1) { 
+        	$raw = new Raw();
+        	$raw->setRaw('apt-get update');
+	}
+
+        if ($step == 2) {
+                $raw = new Raw();
+                $raw->setRaw('apt-get upgrade -y');
+        }
+
+        if ($step == 3) {
+                $raw = new Raw();
+                $raw->setRaw('apt-get install -y python-apt aptitude');
+        }
 
         $playbook->setTask($raw->toArray());
 
