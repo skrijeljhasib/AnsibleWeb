f7a8307bcce1b1ef9a8c1e0d3757e8b3eeb97058
lav <lvenier@flash-global.net>
2017-07-15 08:12:24 +0200

    home fix and connect cli issue logs

--- app/config/routing.php
+++ app/config/routing.php
@@ -5,11 +5,11 @@ use Project\Action\Home;
 use Project\Action\CreateMachine;
 use Project\Action\ListMachine;
 use Project\Action\GetAllMachine;
+use ObjectivePHP\Router\Config\UrlAlias;
 
 return [
         // route aliasing
-        new FastRoute('home', '/', Home::class, FastRoute::GET),
         new FastRoute('createMachine', '/createMachine', CreateMachine::class, FastRoute::GET),
         new FastRoute('listMachine', '/listMachine', ListMachine::class, FastRoute::GET),
-	new FastRoute('getAllMachine', '/getAllMachine', ListMachine::class, FastRoute::GET),
+	new UrlAlias('/', '/listMachine')
 ];
